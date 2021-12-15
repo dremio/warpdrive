735b32966bdd902cab069b2fc19560d2b96ec8a9
James Duong <duong.james@gmail.com>
2021-12-15 11:17:43 -0800

    Remove and gitignore CMake generated files

--- CMakeFiles/iwyu.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/iwyu"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/iwyu.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- CMakeFiles/warpdrive_dependencies.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/warpdrive_dependencies.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
