470c3a41f0d15399f97239332bfb1e6755e5a3b7
James Duong <jduong@dremio.com>
2022-01-26 09:55:55 -0800

    WIP: Wire ResultSetMetadata to warpdrive
    - Implement support for descriptors, including handling of implicit descriptors created
    with the statement handle and explicit descriptors created using SQLAllocHandle.
    - Implement behavior from ODBC spec to revert to implicit statement descriptors if
    an explicit descriptor that is registered to a statement is freed.
    - Add methods for converting attributes and fields to ODBC buffers
    - Add methos for converting between SQLCHAR/SQLWCHAR data
    
    TODO:
    - Propagate Set/GetDec Field + Rec methods to ODBCDescriptor
    - Wire SQLColAttribute to read from IRD fields.
    
    Change-Id: I7b24ec962bbead869e798bc65e6713eb7b0af0fe

--- src/warpdrive/ODBCStatement.cc
+++ src/warpdrive/ODBCStatement.cc
@@ -6,20 +6,39 @@
 #include "ODBCStatement.h"
 
 #include "ODBCConnection.h"
+#include "ODBCDescriptor.h"
+#include "sql.h"
 #include <odbcabstraction/statement.h>
 #include <odbcabstraction/exceptions.h>
+#include <odbcabstraction/result_set.h>
+#include <odbcabstraction/result_set_metadata.h>
 #include <boost/optional.hpp>
 
 using namespace ODBC;
 using namespace driver::odbcabstraction;
+
+namespace {
+  void DescriptorToHandle(SQLPOINTER output, ODBCDescriptor* descriptor, SQLLEN* lenPtr) {
+    if (output) {
+      SQLHANDLE* outputHandle = static_cast<SQLHANDLE*>(output);
+      *outputHandle = reinterpret_cast<SQLHANDLE>(descriptor);
+      *lenPtr = sizeof(SQLHANDLE);
+    }
+  }
+}
 
 // Public =========================================================================================
 ODBCStatement::ODBCStatement(ODBCConnection& connection, 
   std::shared_ptr<driver::odbcabstraction::Statement> spiStatement) :
   m_connection(connection),
   m_spiStatement(spiStatement),
+  m_builtInArd(std::make_shared<ODBCDescriptor>(nullptr, true, true)),
+  m_builtInApd(std::make_shared<ODBCDescriptor>(nullptr, true, true)),
+  m_ipd(std::make_shared<ODBCDescriptor>(nullptr, false, true)),
+  m_ird(std::make_shared<ODBCDescriptor>(nullptr, false, false)),
+  m_currentArd(m_builtInApd.get()),
+  m_currentApd(m_builtInApd.get()),
   m_isPrepared(false) {
-
 }
     
 bool ODBCStatement::isPrepared() const {
@@ -29,7 +48,9 @@ bool ODBCStatement::isPrepared() const {
 void ODBCStatement::Prepare(const std::string& query) {
   boost::optional<std::shared_ptr<ResultSetMetadata> > metadata = m_spiStatement->Prepare(query);
 
-  // TODO: Apply the ResultSetMetaData to the IRD as part of wiring result set metadata.
+  if (metadata) {
+    m_ird->PopulateFromResultSetMetadata(metadata->get());
+  }
   m_isPrepared = true;
 }
 
@@ -38,14 +59,15 @@ void ODBCStatement::ExecutePrepared() {
     throw DriverException("Function sequence error");
   }
 
-  m_spiStatement->ExecutePrepared();
-
-  // TODO: Apply the ResultSetMetaData to the IRD as part of wiring result set metadata.
-  m_isPrepared = true;
+  if (m_spiStatement->ExecutePrepared()) {
+    m_ird->PopulateFromResultSetMetadata(m_spiStatement->GetResultSet()->GetMetadata().get());
+  }
 }
 
 void ODBCStatement::ExecuteDirect(const std::string& query) {
-  m_spiStatement->Execute(query);
+  if (m_spiStatement->Execute(query)) {
+    m_ird->PopulateFromResultSetMetadata(m_spiStatement->GetResultSet()->GetMetadata().get());
+  }
 
   // Direct execution wipes out the prepared state.
   m_isPrepared = false;
@@ -57,6 +79,66 @@ bool ODBCStatement::Fetch(size_t rows) {
 
   // Then call Move(rows) on the ResultSet.
   return false;
+}
+
+void ODBCStatement::GetAttribute(SQLINTEGER statementAttribute, SQLPOINTER output, SQLLEN bufferSize, SQLLEN* strLenPtr) {
+  switch (statementAttribute) {
+    case SQL_ATTR_APP_PARAM_DESC:
+      DescriptorToHandle(output, m_currentApd, strLenPtr);
+      break;
+    case SQL_ATTR_APP_ROW_DESC:
+      DescriptorToHandle(output, m_currentArd, strLenPtr);
+      break;
+    case SQL_ATTR_IMP_PARAM_DESC:
+      DescriptorToHandle(output, m_ipd.get(), strLenPtr);
+      break;
+    case SQL_ATTR_IMP_ROW_DESC:
+      DescriptorToHandle(output, m_ird.get(), strLenPtr);
+      break;
+  }
+}
+
+void ODBCStatement::SetAttribute(SQLINTEGER statementAttribute, SQLPOINTER value, SQLLEN bufferSize) {
+  switch (statementAttribute) {
+    case SQL_ATTR_APP_PARAM_DESC: {
+        ODBCDescriptor* desc = static_cast<ODBCDescriptor*>(value);
+        if (m_currentApd != desc) {
+          if (m_currentApd != m_builtInApd.get()) {
+            m_currentApd->DetachFromStatement(this, true);
+          }
+          m_currentApd = desc;
+          if (m_currentApd != m_builtInApd.get()) {
+            desc->RegisterToStatement(this, true);
+          }
+        }
+        break;
+      }
+    case SQL_ATTR_APP_ROW_DESC: {
+        ODBCDescriptor* desc = static_cast<ODBCDescriptor*>(value);
+        if (m_currentArd != desc) {
+          if (m_currentArd != m_builtInArd.get()) {
+            m_currentArd->DetachFromStatement(this, false);
+          }
+          m_currentArd = desc;
+          if (m_currentArd != m_builtInArd.get()) {
+            desc->RegisterToStatement(this, false);
+          }
+        }
+        break;
+      }
+    case SQL_ATTR_IMP_PARAM_DESC:
+      throw DriverException("Cannot assign impl descriptor.");
+    case SQL_ATTR_IMP_ROW_DESC:
+      throw DriverException("Cannot assign impl descriptor.");
+  }
+}
+
+void ODBCStatement::RevertAppDescriptor(bool isApd) {
+  if (isApd) {
+    m_currentApd = m_builtInApd.get();
+  } else {
+    m_currentArd = m_builtInArd.get();
+  }
 }
 
 void ODBCStatement::closeCursor(bool suppressErrors) {
