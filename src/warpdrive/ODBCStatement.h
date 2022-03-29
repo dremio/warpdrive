9abbbcb6f9a843ff6c13acdf4d5e54044be44e8a
James Duong <jduong@dremio.com>
2022-03-29 14:13:24 -0700

    Statement attributes
    Implement support for SQLSet/GetStmtAtrW
    - Supports setting ODBC 2.x statement attributes on the connection
    - Supports setting descriptor header and record fields using statement attributes
    - Support accessing descriptor handles using statement attributes
    
    Also add missed change to use dynamically linked OpenSSL on Windows to
    fix build issues. Fix build issues on Windows introduced with some
    Linux fixes.
    
    Change-Id: I31d8acc3b2623a3fd643ca9a5038b0bf19008d80

--- src/warpdrive/ODBCStatement.h
+++ src/warpdrive/ODBCStatement.h
@@ -37,6 +37,7 @@ class ODBCStatement {
     
     ~ODBCStatement() = default;
 
+    void CopyAttributesFromConnection(ODBCConnection& connection);
     void Prepare(const std::string& query);
     void ExecutePrepared();
     void ExecuteDirect(const std::string& query);
@@ -47,8 +48,10 @@ class ODBCStatement {
     bool Fetch(size_t rows);
     bool isPrepared() const;
 
-    void GetAttribute(SQLINTEGER statementAttribute, SQLPOINTER output, SQLLEN bufferSize, SQLLEN* strLenPtr);
-    void SetAttribute(SQLINTEGER statementAttribute, SQLPOINTER value, SQLLEN bufferSize);
+    void GetStmtAttr(SQLINTEGER statementAttribute, SQLPOINTER output,
+                     SQLINTEGER bufferSize, SQLINTEGER *strLenPtr, bool isUnicode);
+    void SetStmtAttr(SQLINTEGER statementAttribute, SQLPOINTER value,
+                     SQLINTEGER bufferSize, bool isUnicode);
 
     void RevertAppDescriptor(bool isApd);
 
@@ -88,6 +91,8 @@ class ODBCStatement {
     std::shared_ptr<ODBCDescriptor> m_ird;
     ODBCDescriptor* m_currentArd;
     ODBCDescriptor* m_currentApd;
+    SQLULEN m_rowNumber;
+    SQLULEN m_maxRows;
     bool m_isPrepared;
     bool m_hasReachedEndOfResult;
 };
