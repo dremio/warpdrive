e0766c19c25c88af3c3972bedc47145c6fdca3d8
Vinicius Fraga <vinicius.fraga@dremio.com>
2022-05-23 15:33:32 -0300

    Refactor ColAttribute Test and add license header
    Change-Id: I563589f52d48818b369af504febe8234b16f48dd

--- src/test/src/colattribute-test.cc
+++ src/test/src/colattribute-test.cc
@@ -1,5 +1,8 @@
-/*
- * Test SQLColAttribute
+/*--------
+ * Module:			colattribute-test.cc
+ *
+ * Comments:		See "readme.txt" for copyright and license information.
+ *--------
  */
 
 #include "common.h"
@@ -9,41 +12,39 @@ class SQLColAttributeTest : public ::testing::TestWithParam<char *> {
     void SetUp() override {
         ASSERT_TRUE(test_connect_ext(GetParam()));
 
-        rc_ = SQLAllocHandle(SQL_HANDLE_STMT, conn, &hstmt_);
-        ASSERT_TRUE(SQL_SUCCEEDED(rc_));
+        return_code_ = SQLAllocHandle(SQL_HANDLE_STMT, conn, &handle_stmt_);
+        ASSERT_TRUE(SQL_SUCCEEDED(return_code_));
     }
 
     void TearDown() override {
-        rc_ = SQLFreeStmt(hstmt_, SQL_CLOSE);
-        CHECK_STMT_RESULT(rc_, "SQLFreeStmt failed", hstmt_);
+        return_code_ = SQLFreeStmt(handle_stmt_, SQL_CLOSE);
+        CHECK_STMT_RESULT(return_code_, "SQLFreeStmt failed", handle_stmt_);
         std::string *err_msg = nullptr;
         ASSERT_TRUE(test_disconnect(err_msg));
     }
 
 protected:
-    int rc_{};
-    HSTMT hstmt_ = SQL_NULL_HSTMT;
+    int return_code_{};
+    HSTMT handle_stmt_ = SQL_NULL_HSTMT;
 };
 
 TEST_P(SQLColAttributeTest, ColAttributeTestWithExtraConnOptions) {
-    int return_code;
-    HSTMT handle_stmt = SQL_NULL_HSTMT;
     SQLUSMALLINT i;
     SQLSMALLINT num_cols;
 
-    return_code = SQLAllocHandle(SQL_HANDLE_STMT, conn, &handle_stmt);
-    CHECK_CONN_RESULT(return_code, "failed to allocate stmt handle", conn);
+    return_code_ = SQLAllocHandle(SQL_HANDLE_STMT, conn, &handle_stmt_);
+    CHECK_CONN_RESULT(return_code_, "failed to allocate stmt handle", conn);
 
     // Get column attributes of a simple query.
 
-    // CHANGE-NOTE: Original query was: "SELECT '1'::int AS intcol, 'foobar'::text AS textcol, 'varchar string'::varchar as varcharcol, ''::varchar as empty_varchar_col, 'varchar-5-col'::varchar(5) as varchar5col, '5 days'::interval day to second"
-    return_code = SQLExecDirect(handle_stmt,
-                                (SQLCHAR *) "SELECT CAST('1' AS INTEGER) AS intcol, CAST('foobar' AS VARCHAR) AS textcol, CAST('varchar string' AS VARCHAR) as varcharcol, CAST(''AS VARCHAR) as empty_varchar_col, CAST('varchar-5-col' AS VARCHAR(5)) as varchar5col",
-                                SQL_NTS);
-    CHECK_STMT_RESULT(return_code, "SQLExecDirect failed", handle_stmt);
+    // NOTE: Original query was: "SELECT '1'::int AS intcol, 'foobar'::text AS textcol, 'varchar string'::varchar as varcharcol, ''::varchar as empty_varchar_col, 'varchar-5-col'::varchar(5) as varchar5col, '5 days'::interval day to second"
+    return_code_ = SQLExecDirect(handle_stmt_,
+                                 (SQLCHAR *) "SELECT CAST('1' AS INTEGER) AS intcol, CAST('foobar' AS VARCHAR) AS textcol, CAST('varchar string' AS VARCHAR) as varcharcol, CAST(''AS VARCHAR) as empty_varchar_col, CAST('varchar-5-col' AS VARCHAR(5)) as varchar5col",
+                                 SQL_NTS);
+    CHECK_STMT_RESULT(return_code_, "SQLExecDirect failed", handle_stmt_);
 
-    return_code = SQLNumResultCols(handle_stmt, &num_cols);
-    CHECK_STMT_RESULT(return_code, "SQLNumResultCols failed", handle_stmt);
+    return_code_ = SQLNumResultCols(handle_stmt_, &num_cols);
+    CHECK_STMT_RESULT(return_code_, "SQLNumResultCols failed", handle_stmt_);
 
     std::vector<std::string> expected_names = {"intcol", "textcol", "varcharcol", "empty_varchar_col", "varchar5col"};
     std::vector<std::string> expected_type_names = {"INTEGER", "VARCHAR", "VARCHAR", "VARCHAR", "VARCHAR"};
@@ -54,34 +55,34 @@ TEST_P(SQLColAttributeTest, ColAttributeTestWithExtraConnOptions) {
         char actual_type_name[64];
         SQLLEN actual_octet_length;
 
-        return_code = SQLColAttribute(handle_stmt,
-                                      i,
-                                      SQL_DESC_LABEL,
-                                      actual_column_name,
-                                      sizeof(actual_column_name),
-                                      nullptr,
-                                      nullptr);
-        CHECK_STMT_RESULT(return_code, "SQLColAttribute failed", handle_stmt);
+        return_code_ = SQLColAttribute(handle_stmt_,
+                                       i,
+                                       SQL_DESC_LABEL,
+                                       actual_column_name,
+                                       sizeof(actual_column_name),
+                                       nullptr,
+                                       nullptr);
+        CHECK_STMT_RESULT(return_code_, "SQLColAttribute failed", handle_stmt_);
         EXPECT_EQ(actual_column_name, expected_names[i - 1]);
 
-        return_code = SQLColAttribute(handle_stmt,
-                                      i,
-                                      SQL_DESC_TYPE_NAME,
-                                      actual_type_name,
-                                      sizeof(actual_type_name),
-                                      nullptr,
-                                      nullptr);
-        CHECK_STMT_RESULT(return_code, "SQLColAttribute failed", handle_stmt);
+        return_code_ = SQLColAttribute(handle_stmt_,
+                                       i,
+                                       SQL_DESC_TYPE_NAME,
+                                       actual_type_name,
+                                       sizeof(actual_type_name),
+                                       nullptr,
+                                       nullptr);
+        CHECK_STMT_RESULT(return_code_, "SQLColAttribute failed", handle_stmt_);
         EXPECT_EQ(actual_type_name, expected_type_names[i - 1]);
 
-        return_code = SQLColAttribute(handle_stmt,
-                                      i,
-                                      SQL_DESC_OCTET_LENGTH,
-                                      nullptr,
-                                      SQL_IS_INTEGER,
-                                      nullptr,
-                                      &actual_octet_length);
-        CHECK_STMT_RESULT(return_code, "SQLColAttribute failed", handle_stmt);
+        return_code_ = SQLColAttribute(handle_stmt_,
+                                       i,
+                                       SQL_DESC_OCTET_LENGTH,
+                                       nullptr,
+                                       SQL_IS_INTEGER,
+                                       nullptr,
+                                       &actual_octet_length);
+        CHECK_STMT_RESULT(return_code_, "SQLColAttribute failed", handle_stmt_);
         EXPECT_EQ(actual_octet_length, expected_octet_length[i - 1]);
     }
 }
