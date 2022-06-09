70d4ec9af22a4a2023d43eae6c8452a2a573d27b
Alex McRae (CW) <alex.mcrae@dremio.com>
2022-06-09 18:45:34 +0000

    Revert "Port Result Conversion tests"
    This reverts commit 745aac4a1fae9866da7d03458b3d0904c300e728.
    
    Reason for revert: Breaks windows build
    
    Change-Id: I9cdc0ee4cf058451e1ff36f8b3324501a1d29bdb

--- src/test/src/common.cc
+++ src/test/src/common.cc
@@ -12,7 +12,7 @@ SQLHDBC conn;
 
 std::string
 format_diagnostic(const std::string& msg, SQLSMALLINT htype, SQLHANDLE handle) {
-	return msg + " " + get_diagnostic(handle, htype);
+	return msg + get_diagnostic(handle, htype);
 }
 
 std::string
