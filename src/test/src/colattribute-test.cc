70d87d972c812be822299b51fc02bc6a3a07422f
Alex McRae <alex.mcrae@dremio.com>
2022-06-14 09:58:45 -0700

    Implement returning concise types
    Change-Id: I3d3991064872106fe860d130b5cd8803fbc7b1ec

--- src/test/src/colattribute-test.cc
+++ src/test/src/colattribute-test.cc
@@ -8,11 +8,31 @@
 
 #include "common.h"
 #include <gtest/gtest.h>
+#include <iostream>
+#include <string>
 
-class SQLColAttributeTest : public ::testing::TestWithParam<char *> {
+struct SQLColAttributeTestParams {
+    std::string m_connectionOptions;
+    uint32_t m_odbcVersion;
+
+    friend std::ostream& operator<< (std::ostream& out, const SQLColAttributeTestParams& params);
+};
+
+std::ostream& operator<< (std::ostream& out, const SQLColAttributeTestParams& params) {
+    out << "{ "
+    << "Connection Options: " << params.m_connectionOptions 
+    << ", Version: " << params.m_odbcVersion
+    << "}";
+
+    return out;
+}
+
+class SQLColAttributeTest : public ::testing::TestWithParam<SQLColAttributeTestParams> {
     void SetUp() override {
+        const SQLColAttributeTestParams params = GetParam();
         std::string err_msg;
-        ASSERT_TRUE(test_connect_ext(GetParam(), &err_msg)) << err_msg;
+        ASSERT_TRUE(test_connect_ext(params.m_connectionOptions.c_str(), &err_msg, params.m_odbcVersion)) << err_msg;
+        connected_ = true;
 
         return_code_ = SQLAllocHandle(SQL_HANDLE_STMT, conn, &handle_stmt_);
         ASSERT_TRUE(SQL_SUCCEEDED(return_code_)) << "Failed to allocate handle in setup";
@@ -41,69 +61,158 @@ TEST_P(SQLColAttributeTest, ColAttributeTestWithExtraConnOptions) {
     SQLUSMALLINT i;
     SQLSMALLINT num_cols;
 
+    const SQLColAttributeTestParams params = GetParam();
+
     return_code_ = SQLAllocHandle(SQL_HANDLE_STMT, conn, &handle_stmt_);
     CHECK_CONN_RESULT(return_code_, "failed to allocate stmt handle", conn);
 
     // Get column attributes of a simple query.
 
     // NOTE: Original query was: "SELECT '1'::int AS intcol, 'foobar'::text AS textcol, 'varchar string'::varchar as varcharcol, ''::varchar as empty_varchar_col, 'varchar-5-col'::varchar(5) as varchar5col, '5 days'::interval day to second"
+    // TODO: add interval type to test concise types
     return_code_ = SQLExecDirect(handle_stmt_,
-                                 (SQLCHAR *) "SELECT CAST('1' AS INTEGER) AS intcol, CAST('foobar' AS VARCHAR) AS textcol, CAST('varchar string' AS VARCHAR) as varcharcol, CAST(''AS VARCHAR) as empty_varchar_col, CAST('varchar-5-col' AS VARCHAR(5)) as varchar5col",
+                                 (SQLCHAR *) "SELECT "
+                                 "CAST('1' AS INTEGER) AS intcol, "
+                                 "CAST('foobar' AS VARCHAR) AS textcol, "
+                                 "CAST('varchar string' AS VARCHAR) as varcharcol, "
+                                 "CAST(''AS VARCHAR) as empty_varchar_col, "
+                                 "CAST('varchar-5-col' AS VARCHAR(5)) as varchar5col, "
+                                 "CAST('2022-10-06' AS TIMESTAMP) as timestampcol, "
+                                 "CAST('2' AS INTERVAL DAY) as intervaldaycol"
+                                 ,
                                  SQL_NTS);
     CHECK_STMT_RESULT(return_code_, "SQLExecDirect failed", handle_stmt_);
 
     return_code_ = SQLNumResultCols(handle_stmt_, &num_cols);
     CHECK_STMT_RESULT(return_code_, "SQLNumResultCols failed", handle_stmt_);
 
-    std::vector<std::string> expected_names = {"intcol", "textcol", "varcharcol", "empty_varchar_col", "varchar5col"};
-    std::vector<std::string> expected_type_names = {"INTEGER", "VARCHAR", "VARCHAR", "VARCHAR", "VARCHAR"};
-    std::vector<long> expected_octet_length = {4, 65536, 65536, 65536, 65536};
+    std::vector<std::string> expected_names = {"intcol", "textcol", "varcharcol", "empty_varchar_col", "varchar5col", "timestampcol", "intervaldaycol"};
+    std::vector<std::string> expected_type_names = {"INTEGER", "VARCHAR", "VARCHAR", "VARCHAR", "VARCHAR", "SQL_DATETIME", "SQL_INTERVAL"};
+    std::vector<long> expected_octet_lengths = {4, 65536, 65536, 65536, 65536, 1024, 1024};
+
+    std::vector<SQLULEN> expected_concise_types_odbc_v2 = {SQL_INTEGER, SQL_VARCHAR, SQL_VARCHAR, SQL_VARCHAR, SQL_VARCHAR, SQL_TIMESTAMP, SQL_INTERVAL_DAY};
+    std::vector<SQLULEN> expected_concise_types_odbc_v3 = {SQL_INTEGER, SQL_VARCHAR, SQL_VARCHAR, SQL_VARCHAR, SQL_VARCHAR, SQL_TYPE_TIMESTAMP, SQL_INTERVAL_DAY};
+
+
+    std::vector<SQLULEN> expected_concise_types;
+    switch (params.m_odbcVersion) {
+        case SQL_OV_ODBC2:
+            expected_concise_types = expected_concise_types_odbc_v2;
+            break;
+        case SQL_OV_ODBC3:
+            expected_concise_types = expected_concise_types_odbc_v3;
+            break;
+        default:
+            FAIL() << "Invalid ODBC Version please check the test parameters";
+    }
 
     for (i = 1; i <= num_cols; i++) {
         char actual_column_name[64];
+        SQLLEN actual_concise_type;
         char actual_type_name[64];
         SQLLEN actual_octet_length;
 
-        return_code_ = SQLColAttribute(handle_stmt_,
-                                       i,
-                                       SQL_DESC_LABEL,
-                                       actual_column_name,
-                                       sizeof(actual_column_name),
-                                       nullptr,
-                                       nullptr);
-        CHECK_STMT_RESULT(return_code_, "SQLColAttribute failed", handle_stmt_);
-        EXPECT_EQ(actual_column_name, expected_names[i - 1]);
+        const std::string expected_column_name = expected_names[i - 1];
+        const SQLLEN expected_concise_type = expected_concise_types[i - 1];
+        // const std::string expected_type_name = expected_type_names[i - 1];
+        const SQLLEN expected_octet_length = expected_octet_lengths[i - 1];
 
-        return_code_ = SQLColAttribute(handle_stmt_,
-                                       i,
-                                       SQL_DESC_TYPE_NAME,
-                                       actual_type_name,
-                                       sizeof(actual_type_name),
-                                       nullptr,
-                                       nullptr);
-        CHECK_STMT_RESULT(return_code_, "SQLColAttribute failed", handle_stmt_);
-        GTEST_SKIP_("DX-52040: Type names currently don't match and DX-52041: Octet length doesn't match.");
-        EXPECT_EQ(actual_type_name, expected_type_names[i - 1]);
+        // SQLColAttribute was only introduced in ODBC 3.0
+        if (params.m_odbcVersion != SQL_OV_ODBC2) {
+            // Check the column name
+            return_code_ = SQLColAttribute(handle_stmt_,
+                                        i,
+                                        SQL_DESC_LABEL,
+                                        actual_column_name,
+                                        sizeof(actual_column_name),
+                                        nullptr,
+                                        nullptr);
+            CHECK_STMT_RESULT(return_code_, "SQLColAttribute failed", handle_stmt_);
+            EXPECT_EQ(actual_column_name, expected_column_name);
 
-        return_code_ = SQLColAttribute(handle_stmt_,
-                                       i,
-                                       SQL_DESC_OCTET_LENGTH,
-                                       nullptr,
-                                       SQL_IS_INTEGER,
-                                       nullptr,
-                                       &actual_octet_length);
-        CHECK_STMT_RESULT(return_code_, "SQLColAttribute failed", handle_stmt_);
-        EXPECT_EQ(actual_octet_length, expected_octet_length[i - 1]);
+            // Check the concise type
+            return_code_ = SQLColAttribute(handle_stmt_,
+                                        i,
+                                        SQL_DESC_CONCISE_TYPE,
+                                        nullptr,
+                                        SQL_IS_INTEGER,
+                                        nullptr,
+                                        &actual_concise_type);
+            CHECK_STMT_RESULT(return_code_, "SQLColAttribute failed", handle_stmt_);
+            EXPECT_EQ(actual_concise_type, expected_concise_type) << "Failure at " + expected_column_name;
+        
+        // // Check the type name
+        // GTEST_SKIP_("DX-50240: Type names are incorrect");
+        // return_code_ = SQLColAttribute(handle_stmt_,
+        //                                i,
+        //                                SQL_DESC_TYPE_NAME,
+        //                                actual_type_name,
+        //                                sizeof(actual_type_name),
+        //                                nullptr,
+        //                                nullptr);
+        // CHECK_STMT_RESULT(return_code_, "SQLColAttribute failed to get SQL_DESC_TYPE_NAME", handle_stmt_);
+        // EXPECT_EQ(actual_type_name, expected_type_names[i - 1]);
+
+            // // Check the octet length
+            // return_code_ = SQLColAttribute(handle_stmt_,
+            //                             i,
+            //                             SQL_DESC_OCTET_LENGTH,
+            //                             nullptr,
+            //                             SQL_IS_INTEGER,
+            //                             nullptr,
+            //                             &actual_octet_length);
+            // CHECK_STMT_RESULT(return_code_, "SQLColAttribute failed to get octet length", handle_stmt_);
+            // EXPECT_EQ(actual_octet_length, expected_octet_length);
+        }
+
+        SQLCHAR actual_column_name_sql_describe[64];
+        SQLSMALLINT actual_data_type;
+        SQLULEN actual_column_size;
+        SQLSMALLINT actual_decimal_size;
+        SQLSMALLINT actual_nullable;
+        
+        return_code_ = SQLDescribeCol(handle_stmt_, i, 
+            actual_column_name_sql_describe, sizeof(actual_column_name_sql_describe), nullptr,
+            &actual_data_type,
+            &actual_column_size,
+            &actual_decimal_size,
+            &actual_nullable
+        );
+        CHECK_STMT_RESULT(return_code_, "SQLDescribeCol failed", handle_stmt_);
+        EXPECT_EQ((char*)actual_column_name_sql_describe, expected_column_name);
+        EXPECT_EQ(actual_data_type, expected_concise_type) << "Failed at column: " + expected_column_name;
+        // EXPECT_EQ(actual_column_size, expected_column_size);
+        // EXPECT_EQ(actual_decimal_size, expected_decimal_size);
+        // EXPECT_EQ(actual_nullable, expected_nullable);
     }
 }
 
-INSTANTIATE_TEST_SUITE_P
-
-(UnknownSizesTest,
+INSTANTIATE_TEST_SUITE_P(UnknownSizesTest,
  SQLColAttributeTest,
  ::testing::Values(
-         (char *) "UnknownSizes=0;MaxVarcharSize=100",
-         (char *) "UnknownSizes=1;MaxVarcharSize=100",
-         (char *) "UnknownSizes=2;MaxVarcharSize=100"
+        SQLColAttributeTestParams {
+            "UnknownSizes=0;MaxVarcharSize=100",
+            SQL_OV_ODBC2
+        },
+        SQLColAttributeTestParams {
+            "UnknownSizes=1;MaxVarcharSize=100",
+            SQL_OV_ODBC2
+        },
+        SQLColAttributeTestParams {
+            "UnknownSizes=2;MaxVarcharSize=100",
+            SQL_OV_ODBC2
+        },
+        SQLColAttributeTestParams {
+            "UnknownSizes=0;MaxVarcharSize=100",
+            SQL_OV_ODBC3
+        },
+        SQLColAttributeTestParams {
+            "UnknownSizes=1;MaxVarcharSize=100",
+            SQL_OV_ODBC3
+        },
+        SQLColAttributeTestParams {
+            "UnknownSizes=2;MaxVarcharSize=100",
+            SQL_OV_ODBC3
+        }
  )
 );
