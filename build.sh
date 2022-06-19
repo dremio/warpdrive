036a9a2a66ae283777146bac7bb3d8ecd5920e61
James Duong <jduong@dremio.com>
2022-06-19 08:02:02 -0700

    Update flightsql-odbc for row status array changes
    Update flightsql-odbc to enable filling of the row status array.
    
    Fix invalid arrow commit ID being used
    
    Change-Id: I154a7684fa1bd7f47bf14de8fb32c3db4342f0d0

--- build.sh
+++ build.sh
@@ -13,13 +13,13 @@ mkdir -p _build
 cd _build
 
 export ARROW_GIT_REPOSITORY=/opt/arrow
-export ARROW_GIT_TAG=6bd50bc8e1a938eeaacd44f00d886dd14a8c4a5b
+export ARROW_GIT_TAG=b050bd0d31db6412256cec3362c0d57c9732e1f2
 
 cmake \
   -GNinja \
   -DOPENSSL_INCLUDE_DIR=/usr/include/openssl \
   -DODBCABSTRACTION_REPO=/opt/flightsql-odbc \
-  -DODBCABSTRACTION_GIT_TAG=6bd50bc8e1a938eeaacd44f00d886dd14a8c4a5b \
+  -DODBCABSTRACTION_GIT_TAG=b31e46ebf4d8ff28b8a73442b516dfbf271eb0fe \
   -DARROW_GIT_REPOSITORY=$ARROW_GIT_REPOSITORY \
   -DARROW_GIT_TAG=$ARROW_GIT_TAG \
   ..
