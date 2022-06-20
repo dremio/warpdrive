08dab579a875ed632ef48e836384aab0138aeca2
jayho <jay.ho@dremio.com>
2022-06-20 16:03:43 -0700

    DX-51624: MacOS Release Pipeline
    - Created build script for osx to facilitate release pipeline
    - Edited existing builds scripts to check if variables are defined before setting them and keep them within local scope
    
    Change-Id: I012c3b0bdebf06d55961cf167afad53f964d5167

---
+++ build_osx.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+#
+# Copyright (C) 2020-2022 Dremio Corporation
+#
+# See “license.txt” for license information.
+
+# This script is to facilitate building the Driver on Mac OSX
+
+set -e
+
+vcpkg/vcpkg install --triplet x64-osx --x-install-root=vcpkg/installed
+
+mkdir -p build
+cd build
+
+ARROW_GIT_REPOSITORY="${ARROW_GIT_REPOSITORY:=https://github.com/apache/arrow}"
+ARROW_GIT_TAG="${ARROW_GIT_TAG:=b050bd0d31db6412256cec3362c0d57c9732e1f2}"
+ODBCABSTRACTION_REPO="${ODBCABSTRACTION_REPO:=../flightsql-odbc}"
+ODBCABSTRACTION_GIT_TAG="${ODBCABSTRACTION_GIT_TAG:=ef90cf8b1f4e649155dff01317578efd87de29bd}"
+
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DVCPKG_TARGET_TRIPLET=x64-osx \
+  -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake \
+  -DARROW_GIT_REPOSITORY=$ARROW_GIT_REPOSITORY \
+  -DARROW_GIT_TAG=$ARROW_GIT_TAG \
+  -DODBCABSTRACTION_REPO=$ODBCABSTRACTION_REPO \
+  -DODBCABSTRACTION_GIT_TAG=$ODBCABSTRACTION_GIT_TAG \
+  -GNinja \
+  -DVCPKG_MANIFEST_MODE=OFF^ \
+  ..
+
+cmake --build . --config Release -j 12 || cmake --build . --config Release -j 12 || cmake --build . --config Release -j 12
+
+cd ..
