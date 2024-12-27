c3ff833765b56f6f1a06ea5f57040b6430018943
Igor Antropov <igor.antropov@dremio.com>
2024-12-27 12:54:11 +0000

    fix other tests

--- src/test/src/statement-functions-test.cc
+++ src/test/src/statement-functions-test.cc
@@ -40,11 +40,11 @@ TEST_F(SQLStatementFunctionsTest, TestExecDirectNormalFlow){
   CHECK_STMT_RESULT(return_code_, "SQLExecDirect failed", hstmt_);
 
   std::string err_msg;
-  std::string exp_result = "50\tCustomer#000000050\t9SzDYlkzxByyJ1QeTI o\t6\t16-658-112-3221\t4266.130000\tMACHINERY \tts. furiously ironic accounts cajole furiously slyly ironic dinos.\n"
-                           "51\tCustomer#000000051\tuR,wEaiTvo4\t12\t22-344-885-4251\t855.870000\tFURNITURE \teposits. furiously regular requests integrate carefully packages. furious\n"
-                           "52\tCustomer#000000052\t7 QOqGqqSy9jfV51BC71jcHJSD0\t11\t21-186-284-5998\t5630.280000\tHOUSEHOLD \tic platelets use evenly even accounts. stealthy theodolites cajole furiou\n"
-                           "53\tCustomer#000000053\tHnaxHzTfFTZs8MuCpJyTbZ47Cm4wFOOgib\t15\t25-168-852-5363\t4113.640000\tHOUSEHOLD \tar accounts are. even foxes are blithely. fluffily pending deposits boost\n"
-                           "54\tCustomer#000000054\t,k4vf 5vECGWFy,hosTE,\t4\t14-776-370-4745\t868.900000\tAUTOMOBILE\tsual, silent accounts. furiously express accounts cajole special deposits. final, final accounts use furi\n";
+  std::string exp_result = "50\tCustomer#000000050\t9SzDYlkzxByyJ1QeTI o\t6\t16-658-112-3221\t4266.130000\tMACHINERY \tts. furiously ironic accounts cajole fu\n"
+                           "51\tCustomer#000000051\tuR,wEaiTvo4\t12\t22-344-885-4251\t855.870000\tFURNITURE \teposits. furiously regular requests int\n"
+                           "52\tCustomer#000000052\t7 QOqGqqSy9jfV51BC71jcHJSD0\t11\t21-186-284-5998\t5630.280000\tHOUSEHOLD \tic platelets use evenly even accounts. \n"
+                           "53\tCustomer#000000053\tHnaxHzTfFTZs8MuCpJyTbZ47Cm4wFOOgib\t15\t25-168-852-5363\t4113.640000\tHOUSEHOLD \tar accounts are. even foxes are blithel\n"
+                           "54\tCustomer#000000054\t,k4vf 5vECGWFy,hosTE,\t4\t14-776-370-4745\t868.900000\tAUTOMOBILE\tsual, silent accounts. furiously expres\n";
   auto result = get_result(hstmt_, &err_msg);
   EXPECT_EQ(exp_result, result.value());
 }
@@ -54,16 +54,16 @@ TEST_F(SQLStatementFunctionsTest, TestExecDirectPrepareBeforeDirectExec){
   return_code_ = SQLPrepare(hstmt_, (SQLCHAR *) sqlQueryPrepared.c_str(), sqlQueryPrepared.length());
   CHECK_STMT_RESULT(return_code_, "SQLPrepare failed", hstmt_);
 
-  std::string sqlQuery = "SELECT * FROM postgres.tpch.customer WHERE c_custkey BETWEEN 536796 AND 536800 LIMIT 5;";
+  std::string sqlQuery = "SELECT * FROM postgres.tpch.customer WHERE c_custkey BETWEEN 50 AND 54 LIMIT 5;";
   return_code_= SQLExecDirect(hstmt_, (SQLCHAR *) sqlQuery.c_str(), sqlQuery.length());
   CHECK_STMT_RESULT(return_code_, "SQLExecDirect failed", hstmt_);
 
   std::string err_msg;
-  std::string exp_result = "50\tCustomer#000000050\t9SzDYlkzxByyJ1QeTI o\t6\t16-658-112-3221\t4266.130000\tMACHINERY \tts. furiously ironic accounts cajole furiously slyly ironic dinos.\n"
-                             "51\tCustomer#000000051\tuR,wEaiTvo4\t12\t22-344-885-4251\t855.870000\tFURNITURE \teposits. furiously regular requests integrate carefully packages. furious\n"
-                             "52\tCustomer#000000052\t7 QOqGqqSy9jfV51BC71jcHJSD0\t11\t21-186-284-5998\t5630.280000\tHOUSEHOLD \tic platelets use evenly even accounts. stealthy theodolites cajole furiou\n"
-                             "53\tCustomer#000000053\tHnaxHzTfFTZs8MuCpJyTbZ47Cm4wFOOgib\t15\t25-168-852-5363\t4113.640000\tHOUSEHOLD \tar accounts are. even foxes are blithely. fluffily pending deposits boost\n"
-                             "54\tCustomer#000000054\t,k4vf 5vECGWFy,hosTE,\t4\t14-776-370-4745\t868.900000\tAUTOMOBILE\tsual, silent accounts. furiously express accounts cajole special deposits. final, final accounts use furi\n";
+  std::string exp_result = "50\tCustomer#000000050\t9SzDYlkzxByyJ1QeTI o\t6\t16-658-112-3221\t4266.130000\tMACHINERY \tts. furiously ironic accounts cajole fu\n"
+                             "51\tCustomer#000000051\tuR,wEaiTvo4\t12\t22-344-885-4251\t855.870000\tFURNITURE \teposits. furiously regular requests int\n"
+                             "52\tCustomer#000000052\t7 QOqGqqSy9jfV51BC71jcHJSD0\t11\t21-186-284-5998\t5630.280000\tHOUSEHOLD \tic platelets use evenly even accounts. \n"
+                             "53\tCustomer#000000053\tHnaxHzTfFTZs8MuCpJyTbZ47Cm4wFOOgib\t15\t25-168-852-5363\t4113.640000\tHOUSEHOLD \tar accounts are. even foxes are blithel\n"
+                             "54\tCustomer#000000054\t,k4vf 5vECGWFy,hosTE,\t4\t14-776-370-4745\t868.900000\tAUTOMOBILE\tsual, silent accounts. furiously expres\n";
   auto result = get_result(hstmt_, &err_msg);
   EXPECT_EQ(exp_result, result.value());
 }
@@ -90,7 +90,7 @@ TEST_F(SQLStatementFunctionsTest, TestExecDirectEarlyClose){
 }
 
 TEST_F(SQLStatementFunctionsTest, TestNormalFlow){
-  std::string sqlQuery = "SELECT * FROM postgres.tpch.customer WHERE c_custkey BETWEEN 536796 AND 536800 LIMIT 5;";
+  std::string sqlQuery = "SELECT * FROM postgres.tpch.customer WHERE c_custkey BETWEEN 50 AND 54 LIMIT 5;";
   return_code_ = SQLPrepare(hstmt_, (SQLCHAR *) sqlQuery.c_str(), sqlQuery.length());
   CHECK_STMT_RESULT(return_code_, "SQLPrepare failed", hstmt_);
 
@@ -98,11 +98,11 @@ TEST_F(SQLStatementFunctionsTest, TestNormalFlow){
   CHECK_STMT_RESULT(return_code_, "SQLExecute failed", hstmt_);
 
   std::string err_msg;
-  std::string exp_result = "50\tCustomer#000000050\t9SzDYlkzxByyJ1QeTI o\t6\t16-658-112-3221\t4266.130000\tMACHINERY \tts. furiously ironic accounts cajole furiously slyly ironic dinos.\n"
-                             "51\tCustomer#000000051\tuR,wEaiTvo4\t12\t22-344-885-4251\t855.870000\tFURNITURE \teposits. furiously regular requests integrate carefully packages. furious\n"
-                             "52\tCustomer#000000052\t7 QOqGqqSy9jfV51BC71jcHJSD0\t11\t21-186-284-5998\t5630.280000\tHOUSEHOLD \tic platelets use evenly even accounts. stealthy theodolites cajole furiou\n"
-                             "53\tCustomer#000000053\tHnaxHzTfFTZs8MuCpJyTbZ47Cm4wFOOgib\t15\t25-168-852-5363\t4113.640000\tHOUSEHOLD \tar accounts are. even foxes are blithely. fluffily pending deposits boost\n"
-                             "54\tCustomer#000000054\t,k4vf 5vECGWFy,hosTE,\t4\t14-776-370-4745\t868.900000\tAUTOMOBILE\tsual, silent accounts. furiously express accounts cajole special deposits. final, final accounts use furi\n";
+  std::string exp_result = "50\tCustomer#000000050\t9SzDYlkzxByyJ1QeTI o\t6\t16-658-112-3221\t4266.130000\tMACHINERY \tts. furiously ironic accounts cajole fu\n"
+                             "51\tCustomer#000000051\tuR,wEaiTvo4\t12\t22-344-885-4251\t855.870000\tFURNITURE \teposits. furiously regular requests int\n"
+                             "52\tCustomer#000000052\t7 QOqGqqSy9jfV51BC71jcHJSD0\t11\t21-186-284-5998\t5630.280000\tHOUSEHOLD \tic platelets use evenly even accounts. \n"
+                             "53\tCustomer#000000053\tHnaxHzTfFTZs8MuCpJyTbZ47Cm4wFOOgib\t15\t25-168-852-5363\t4113.640000\tHOUSEHOLD \tar accounts are. even foxes are blithel\n"
+                             "54\tCustomer#000000054\t,k4vf 5vECGWFy,hosTE,\t4\t14-776-370-4745\t868.900000\tAUTOMOBILE\tsual, silent accounts. furiously expres\n";
   auto result = get_result(hstmt_, &err_msg);
   EXPECT_EQ(exp_result, result.value());
 }
@@ -134,7 +134,7 @@ TEST_F(SQLStatementFunctionsTest, TestEarlyClose){
 }
 
 TEST_F(SQLStatementFunctionsTest, TestStatementReuse){
-  std::string sqlQuery = "SELECT * FROM postgres.tpch.customer WHERE c_custkey BETWEEN 536796 AND 536800 LIMIT 5;";
+  std::string sqlQuery = "SELECT * FROM postgres.tpch.customer WHERE c_custkey BETWEEN 50 AND 54 LIMIT 5;";
   return_code_ = SQLPrepare(hstmt_, (SQLCHAR *) sqlQuery.c_str(), sqlQuery.length());
   CHECK_STMT_RESULT(return_code_, "SQLPrepare failed", hstmt_);
 
@@ -148,22 +148,22 @@ TEST_F(SQLStatementFunctionsTest, TestStatementReuse){
   CHECK_STMT_RESULT(return_code_, "SQLExecute failed", hstmt_);
 
   std::string err_msg;
-  std::string exp_result = "50\tCustomer#000000050\t9SzDYlkzxByyJ1QeTI o\t6\t16-658-112-3221\t4266.130000\tMACHINERY \tts. furiously ironic accounts cajole furiously slyly ironic dinos.\n"
-                             "51\tCustomer#000000051\tuR,wEaiTvo4\t12\t22-344-885-4251\t855.870000\tFURNITURE \teposits. furiously regular requests integrate carefully packages. furious\n"
-                             "52\tCustomer#000000052\t7 QOqGqqSy9jfV51BC71jcHJSD0\t11\t21-186-284-5998\t5630.280000\tHOUSEHOLD \tic platelets use evenly even accounts. stealthy theodolites cajole furiou\n"
-                             "53\tCustomer#000000053\tHnaxHzTfFTZs8MuCpJyTbZ47Cm4wFOOgib\t15\t25-168-852-5363\t4113.640000\tHOUSEHOLD \tar accounts are. even foxes are blithely. fluffily pending deposits boost\n"
-                             "54\tCustomer#000000054\t,k4vf 5vECGWFy,hosTE,\t4\t14-776-370-4745\t868.900000\tAUTOMOBILE\tsual, silent accounts. furiously express accounts cajole special deposits. final, final accounts use furi\n";
+  std::string exp_result = "50\tCustomer#000000050\t9SzDYlkzxByyJ1QeTI o\t6\t16-658-112-3221\t4266.130000\tMACHINERY \tts. furiously ironic accounts cajole fu\n"
+                             "51\tCustomer#000000051\tuR,wEaiTvo4\t12\t22-344-885-4251\t855.870000\tFURNITURE \teposits. furiously regular requests int\n"
+                             "52\tCustomer#000000052\t7 QOqGqqSy9jfV51BC71jcHJSD0\t11\t21-186-284-5998\t5630.280000\tHOUSEHOLD \tic platelets use evenly even accounts. \n"
+                             "53\tCustomer#000000053\tHnaxHzTfFTZs8MuCpJyTbZ47Cm4wFOOgib\t15\t25-168-852-5363\t4113.640000\tHOUSEHOLD \tar accounts are. even foxes are blithel\n"
+                             "54\tCustomer#000000054\t,k4vf 5vECGWFy,hosTE,\t4\t14-776-370-4745\t868.900000\tAUTOMOBILE\tsual, silent accounts. furiously expres\n";
   auto result = get_result(hstmt_, &err_msg);
   EXPECT_EQ(exp_result, result.value());
 }
 
 TEST_F(SQLStatementFunctionsTest, TestStatementReuse2){
-  std::string sqlQuery = "SELECT * FROM postgres.tpch.customer WHERE c_custkey BETWEEN 536796 AND 536800 LIMIT 5;";
-  std::string exp_result = "50\tCustomer#000000050\t9SzDYlkzxByyJ1QeTI o\t6\t16-658-112-3221\t4266.130000\tMACHINERY \tts. furiously ironic accounts cajole furiously slyly ironic dinos.\n"
-                             "51\tCustomer#000000051\tuR,wEaiTvo4\t12\t22-344-885-4251\t855.870000\tFURNITURE \teposits. furiously regular requests integrate carefully packages. furious\n"
-                             "52\tCustomer#000000052\t7 QOqGqqSy9jfV51BC71jcHJSD0\t11\t21-186-284-5998\t5630.280000\tHOUSEHOLD \tic platelets use evenly even accounts. stealthy theodolites cajole furiou\n"
-                             "53\tCustomer#000000053\tHnaxHzTfFTZs8MuCpJyTbZ47Cm4wFOOgib\t15\t25-168-852-5363\t4113.640000\tHOUSEHOLD \tar accounts are. even foxes are blithely. fluffily pending deposits boost\n"
-                             "54\tCustomer#000000054\t,k4vf 5vECGWFy,hosTE,\t4\t14-776-370-4745\t868.900000\tAUTOMOBILE\tsual, silent accounts. furiously express accounts cajole special deposits. final, final accounts use furi\n";
+  std::string sqlQuery = "SELECT * FROM postgres.tpch.customer WHERE c_custkey BETWEEN 50 AND 54 LIMIT 5;";
+  std::string exp_result = "50\tCustomer#000000050\t9SzDYlkzxByyJ1QeTI o\t6\t16-658-112-3221\t4266.130000\tMACHINERY \tts. furiously ironic accounts cajole fu\n"
+                             "51\tCustomer#000000051\tuR,wEaiTvo4\t12\t22-344-885-4251\t855.870000\tFURNITURE \teposits. furiously regular requests int\n"
+                             "52\tCustomer#000000052\t7 QOqGqqSy9jfV51BC71jcHJSD0\t11\t21-186-284-5998\t5630.280000\tHOUSEHOLD \tic platelets use evenly even accounts. \n"
+                             "53\tCustomer#000000053\tHnaxHzTfFTZs8MuCpJyTbZ47Cm4wFOOgib\t15\t25-168-852-5363\t4113.640000\tHOUSEHOLD \tar accounts are. even foxes are blithel\n"
+                             "54\tCustomer#000000054\t,k4vf 5vECGWFy,hosTE,\t4\t14-776-370-4745\t868.900000\tAUTOMOBILE\tsual, silent accounts. furiously expres\n";
 
   return_code_ = SQLPrepare(hstmt_, (SQLCHAR *) sqlQuery.c_str(), sqlQuery.length());
   CHECK_STMT_RESULT(return_code_, "SQLPrepare failed", hstmt_);
