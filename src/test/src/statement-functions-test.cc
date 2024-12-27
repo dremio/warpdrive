9358ad2698ac03593636d02dea8be16c2ace8957
Igor Antropov <igor.antropov@dremio.com>
2024-12-27 10:44:27 +0000

    DX-90582: Fix tests

--- src/test/src/statement-functions-test.cc
+++ src/test/src/statement-functions-test.cc
@@ -35,16 +35,16 @@ protected:
 };
 
 TEST_F(SQLStatementFunctionsTest, TestExecDirectNormalFlow){
-  std::string sqlQuery = "SELECT * FROM postgres.tpch.customer WHERE c_custkey BETWEEN 536796 AND 536800 LIMIT 5;";
+  std::string sqlQuery = "SELECT * FROM postgres.tpch.customer WHERE c_custkey BETWEEN 50 AND 54 LIMIT 5;";
   return_code_= SQLExecDirect(hstmt_, (SQLCHAR *) sqlQuery.c_str(), sqlQuery.length());
   CHECK_STMT_RESULT(return_code_, "SQLExecDirect failed", hstmt_);
 
   std::string err_msg;
-  std::string exp_result = "536796\tCustomer#000536796\tUWHFXVJzZMixLpz00Mk1r,PfP3LT DuLeO\t10\t20-352-489-5961\t5193.040000\tMACHINERY \t beans run after the carefully pending \n"
-                           "536797\tCustomer#000536797\t51QNmdLF0ZR\t7\t17-492-119-8512\t3379.690000\tAUTOMOBILE\tkly regular accounts sleep even, regula\n"
-                           "536798\tCustomer#000536798\tvoBZynUBhAU\t3\t13-564-584-3770\t7504.490000\tHOUSEHOLD \te carefully pinto beans. slyly regular \n"
-                           "536799\tCustomer#000536799\txhD,AtTcEDMnXQroS11O0APgjW3t\t5\t15-958-448-6750\t-37.990000\tMACHINERY \tn pinto beans affix slyly quickly ironi\n"
-                           "536800\tCustomer#000536800\t8lnRHiYEDTSNryX\t22\t32-318-475-1634\t8418.260000\tMACHINERY \tickly regular pinto beans cajole furiou\n";
+  std::string exp_result = "50\tCustomer#000000050\t9SzDYlkzxByyJ1QeTI o\t6\t16-658-112-3221\t4266.130000\tMACHINERY \tts. furiously ironic accounts cajole furiously slyly ironic dinos.\n"
+                           "51\tCustomer#000000051\tuR,wEaiTvo4\t12\t22-344-885-4251\t855.870000\tFURNITURE \teposits. furiously regular requests integrate carefully packages. furious\n"
+                           "52\tCustomer#000000052\t7 QOqGqqSy9jfV51BC71jcHJSD0\t11\t21-186-284-5998\t5630.280000\tHOUSEHOLD \tic platelets use evenly even accounts. stealthy theodolites cajole furiou\n"
+                           "53\tCustomer#000000053\tHnaxHzTfFTZs8MuCpJyTbZ47Cm4wFOOgib\t15\t25-168-852-5363\t4113.640000\tHOUSEHOLD \tar accounts are. even foxes are blithely. fluffily pending deposits boost\n"
+                           "54\tCustomer#000000054\t,k4vf 5vECGWFy,hosTE,\t4\t14-776-370-4745\t868.900000\tAUTOMOBILE\tsual, silent accounts. furiously express accounts cajole special deposits. final, final accounts use furi\n";
   auto result = get_result(hstmt_, &err_msg);
   EXPECT_EQ(exp_result, result.value());
 }
@@ -59,11 +59,11 @@ TEST_F(SQLStatementFunctionsTest, TestExecDirectPrepareBeforeDirectExec){
   CHECK_STMT_RESULT(return_code_, "SQLExecDirect failed", hstmt_);
 
   std::string err_msg;
-  std::string exp_result = "536796\tCustomer#000536796\tUWHFXVJzZMixLpz00Mk1r,PfP3LT DuLeO\t10\t20-352-489-5961\t5193.040000\tMACHINERY \t beans run after the carefully pending \n"
-                           "536797\tCustomer#000536797\t51QNmdLF0ZR\t7\t17-492-119-8512\t3379.690000\tAUTOMOBILE\tkly regular accounts sleep even, regula\n"
-                           "536798\tCustomer#000536798\tvoBZynUBhAU\t3\t13-564-584-3770\t7504.490000\tHOUSEHOLD \te carefully pinto beans. slyly regular \n"
-                           "536799\tCustomer#000536799\txhD,AtTcEDMnXQroS11O0APgjW3t\t5\t15-958-448-6750\t-37.990000\tMACHINERY \tn pinto beans affix slyly quickly ironi\n"
-                           "536800\tCustomer#000536800\t8lnRHiYEDTSNryX\t22\t32-318-475-1634\t8418.260000\tMACHINERY \tickly regular pinto beans cajole furiou\n";
+  std::string exp_result = "50\tCustomer#000000050\t9SzDYlkzxByyJ1QeTI o\t6\t16-658-112-3221\t4266.130000\tMACHINERY \tts. furiously ironic accounts cajole furiously slyly ironic dinos.\n"
+                             "51\tCustomer#000000051\tuR,wEaiTvo4\t12\t22-344-885-4251\t855.870000\tFURNITURE \teposits. furiously regular requests integrate carefully packages. furious\n"
+                             "52\tCustomer#000000052\t7 QOqGqqSy9jfV51BC71jcHJSD0\t11\t21-186-284-5998\t5630.280000\tHOUSEHOLD \tic platelets use evenly even accounts. stealthy theodolites cajole furiou\n"
+                             "53\tCustomer#000000053\tHnaxHzTfFTZs8MuCpJyTbZ47Cm4wFOOgib\t15\t25-168-852-5363\t4113.640000\tHOUSEHOLD \tar accounts are. even foxes are blithely. fluffily pending deposits boost\n"
+                             "54\tCustomer#000000054\t,k4vf 5vECGWFy,hosTE,\t4\t14-776-370-4745\t868.900000\tAUTOMOBILE\tsual, silent accounts. furiously express accounts cajole special deposits. final, final accounts use furi\n";
   auto result = get_result(hstmt_, &err_msg);
   EXPECT_EQ(exp_result, result.value());
 }
@@ -98,11 +98,11 @@ TEST_F(SQLStatementFunctionsTest, TestNormalFlow){
   CHECK_STMT_RESULT(return_code_, "SQLExecute failed", hstmt_);
 
   std::string err_msg;
-  std::string exp_result = "536796\tCustomer#000536796\tUWHFXVJzZMixLpz00Mk1r,PfP3LT DuLeO\t10\t20-352-489-5961\t5193.040000\tMACHINERY \t beans run after the carefully pending \n"
-                           "536797\tCustomer#000536797\t51QNmdLF0ZR\t7\t17-492-119-8512\t3379.690000\tAUTOMOBILE\tkly regular accounts sleep even, regula\n"
-                           "536798\tCustomer#000536798\tvoBZynUBhAU\t3\t13-564-584-3770\t7504.490000\tHOUSEHOLD \te carefully pinto beans. slyly regular \n"
-                           "536799\tCustomer#000536799\txhD,AtTcEDMnXQroS11O0APgjW3t\t5\t15-958-448-6750\t-37.990000\tMACHINERY \tn pinto beans affix slyly quickly ironi\n"
-                           "536800\tCustomer#000536800\t8lnRHiYEDTSNryX\t22\t32-318-475-1634\t8418.260000\tMACHINERY \tickly regular pinto beans cajole furiou\n";
+  std::string exp_result = "50\tCustomer#000000050\t9SzDYlkzxByyJ1QeTI o\t6\t16-658-112-3221\t4266.130000\tMACHINERY \tts. furiously ironic accounts cajole furiously slyly ironic dinos.\n"
+                             "51\tCustomer#000000051\tuR,wEaiTvo4\t12\t22-344-885-4251\t855.870000\tFURNITURE \teposits. furiously regular requests integrate carefully packages. furious\n"
+                             "52\tCustomer#000000052\t7 QOqGqqSy9jfV51BC71jcHJSD0\t11\t21-186-284-5998\t5630.280000\tHOUSEHOLD \tic platelets use evenly even accounts. stealthy theodolites cajole furiou\n"
+                             "53\tCustomer#000000053\tHnaxHzTfFTZs8MuCpJyTbZ47Cm4wFOOgib\t15\t25-168-852-5363\t4113.640000\tHOUSEHOLD \tar accounts are. even foxes are blithely. fluffily pending deposits boost\n"
+                             "54\tCustomer#000000054\t,k4vf 5vECGWFy,hosTE,\t4\t14-776-370-4745\t868.900000\tAUTOMOBILE\tsual, silent accounts. furiously express accounts cajole special deposits. final, final accounts use furi\n";
   auto result = get_result(hstmt_, &err_msg);
   EXPECT_EQ(exp_result, result.value());
 }
@@ -148,22 +148,22 @@ TEST_F(SQLStatementFunctionsTest, TestStatementReuse){
   CHECK_STMT_RESULT(return_code_, "SQLExecute failed", hstmt_);
 
   std::string err_msg;
-  std::string exp_result = "536796\tCustomer#000536796\tUWHFXVJzZMixLpz00Mk1r,PfP3LT DuLeO\t10\t20-352-489-5961\t5193.040000\tMACHINERY \t beans run after the carefully pending \n"
-                           "536797\tCustomer#000536797\t51QNmdLF0ZR\t7\t17-492-119-8512\t3379.690000\tAUTOMOBILE\tkly regular accounts sleep even, regula\n"
-                           "536798\tCustomer#000536798\tvoBZynUBhAU\t3\t13-564-584-3770\t7504.490000\tHOUSEHOLD \te carefully pinto beans. slyly regular \n"
-                           "536799\tCustomer#000536799\txhD,AtTcEDMnXQroS11O0APgjW3t\t5\t15-958-448-6750\t-37.990000\tMACHINERY \tn pinto beans affix slyly quickly ironi\n"
-                           "536800\tCustomer#000536800\t8lnRHiYEDTSNryX\t22\t32-318-475-1634\t8418.260000\tMACHINERY \tickly regular pinto beans cajole furiou\n";
+  std::string exp_result = "50\tCustomer#000000050\t9SzDYlkzxByyJ1QeTI o\t6\t16-658-112-3221\t4266.130000\tMACHINERY \tts. furiously ironic accounts cajole furiously slyly ironic dinos.\n"
+                             "51\tCustomer#000000051\tuR,wEaiTvo4\t12\t22-344-885-4251\t855.870000\tFURNITURE \teposits. furiously regular requests integrate carefully packages. furious\n"
+                             "52\tCustomer#000000052\t7 QOqGqqSy9jfV51BC71jcHJSD0\t11\t21-186-284-5998\t5630.280000\tHOUSEHOLD \tic platelets use evenly even accounts. stealthy theodolites cajole furiou\n"
+                             "53\tCustomer#000000053\tHnaxHzTfFTZs8MuCpJyTbZ47Cm4wFOOgib\t15\t25-168-852-5363\t4113.640000\tHOUSEHOLD \tar accounts are. even foxes are blithely. fluffily pending deposits boost\n"
+                             "54\tCustomer#000000054\t,k4vf 5vECGWFy,hosTE,\t4\t14-776-370-4745\t868.900000\tAUTOMOBILE\tsual, silent accounts. furiously express accounts cajole special deposits. final, final accounts use furi\n";
   auto result = get_result(hstmt_, &err_msg);
   EXPECT_EQ(exp_result, result.value());
 }
 
 TEST_F(SQLStatementFunctionsTest, TestStatementReuse2){
   std::string sqlQuery = "SELECT * FROM postgres.tpch.customer WHERE c_custkey BETWEEN 536796 AND 536800 LIMIT 5;";
-  std::string exp_result = "536796\tCustomer#000536796\tUWHFXVJzZMixLpz00Mk1r,PfP3LT DuLeO\t10\t20-352-489-5961\t5193.040000\tMACHINERY \t beans run after the carefully pending \n"
-                           "536797\tCustomer#000536797\t51QNmdLF0ZR\t7\t17-492-119-8512\t3379.690000\tAUTOMOBILE\tkly regular accounts sleep even, regula\n"
-                           "536798\tCustomer#000536798\tvoBZynUBhAU\t3\t13-564-584-3770\t7504.490000\tHOUSEHOLD \te carefully pinto beans. slyly regular \n"
-                           "536799\tCustomer#000536799\txhD,AtTcEDMnXQroS11O0APgjW3t\t5\t15-958-448-6750\t-37.990000\tMACHINERY \tn pinto beans affix slyly quickly ironi\n"
-                           "536800\tCustomer#000536800\t8lnRHiYEDTSNryX\t22\t32-318-475-1634\t8418.260000\tMACHINERY \tickly regular pinto beans cajole furiou\n";
+  std::string exp_result = "50\tCustomer#000000050\t9SzDYlkzxByyJ1QeTI o\t6\t16-658-112-3221\t4266.130000\tMACHINERY \tts. furiously ironic accounts cajole furiously slyly ironic dinos.\n"
+                             "51\tCustomer#000000051\tuR,wEaiTvo4\t12\t22-344-885-4251\t855.870000\tFURNITURE \teposits. furiously regular requests integrate carefully packages. furious\n"
+                             "52\tCustomer#000000052\t7 QOqGqqSy9jfV51BC71jcHJSD0\t11\t21-186-284-5998\t5630.280000\tHOUSEHOLD \tic platelets use evenly even accounts. stealthy theodolites cajole furiou\n"
+                             "53\tCustomer#000000053\tHnaxHzTfFTZs8MuCpJyTbZ47Cm4wFOOgib\t15\t25-168-852-5363\t4113.640000\tHOUSEHOLD \tar accounts are. even foxes are blithely. fluffily pending deposits boost\n"
+                             "54\tCustomer#000000054\t,k4vf 5vECGWFy,hosTE,\t4\t14-776-370-4745\t868.900000\tAUTOMOBILE\tsual, silent accounts. furiously express accounts cajole special deposits. final, final accounts use furi\n";
 
   return_code_ = SQLPrepare(hstmt_, (SQLCHAR *) sqlQuery.c_str(), sqlQuery.length());
   CHECK_STMT_RESULT(return_code_, "SQLPrepare failed", hstmt_);
